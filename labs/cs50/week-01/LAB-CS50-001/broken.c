27383cf54e593274830387b566d87cacd72b040a
Lucas Amaral (Trevvos Founder) <lucas.trevvos@gmail.com>
2026-08-21 01:19:58 -0300

    lab: add intentionally broken C repair exercise

---
+++ labs/cs50/week-01/LAB-CS50-001/broken.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int total = 7;
+    int people = 2;
+    float average = total / people;
+
+    int score = 9;
+
+    if (score = 10)
+    {
+        printf("perfect\n");
+    }
+
+    for (int i = 1; i <= 3; i--)
+    {
+        printf("%i\n", i);
+    }
+
+    printf("average: %.1f\n", average);
+
+    return 0;
+}
